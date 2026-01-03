8bcaaa9cbfe51cda9885b625b78dd4cfc3a59af4
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2026-01-03 23:01:29 +0900

    add google metatag

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="ogE7nIzUAEGnSsw3YDTeyfaemBHdxn4X2f555Wq3cUQ" />
 
         <title inertia>ひたすら数学</title>
         <link rel="icon" type="image/png" href="/images/logo.png">
